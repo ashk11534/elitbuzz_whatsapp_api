660dbcdeec105bccb8a7cfd261f40cba4dfa9786
Ashik <ashk11534@gmail.com>
2024-10-21 14:51:56 +0600

    final version 1.2

--- models/models.py
+++ models/models.py
@@ -2,35 +2,18 @@ from odoo import fields, models, api
 import requests
 
 def get_instance_id(access_token):
-    data = {
-        'access_token': access_token
-    }
 
-    headers = {
-        'Content-Type': 'application/json'
-    }
     try:
-        response = requests.post(f'https://api.rangebroadcast.com/api/create_instance', json=data, headers=headers)
+        response = requests.get(f'https://api.rangebroadcast.com/api/create_instance?access_token={access_token}')
         response = response.json()
+        print(response)
         return response.get('instance_id')
     except Exception as e:
         return '6715EDA2DBE4A'
 
 
 def send_message(phone_number, message, instance_id, access_token):
-    data = {
-        'number': phone_number,
-        'message': message,
-        'instance_id': instance_id,
-        'access_token': access_token,
-        'type': 'text'
-    }
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.post(f'https://api.rangebroadcast.com/api/send', json=data, headers=headers)
+    response = requests.get(f'https://api.rangebroadcast.com/api/send?number={phone_number}&type=text&message={message}&instance_id={instance_id}&access_token={access_token}')
     print(response.json())
 
 class CRMWhatsappInherit(models.Model):
@@ -41,11 +24,11 @@ class CRMWhatsappInherit(models.Model):
         phone_number = vals.get('phone') if vals.get('phone') else '8801955477945'
 
         try:
-            instance_id = get_instance_id('670cf52e29b86')
+            instance_id = get_instance_id('670cf52e29b86') # this instance id is not active
 
             message = "Hi, we are from Elitbuzz technologies LTD. A new CRM lead has been created for you."
 
-            send_message(phone_number, message, instance_id, access_token='670cf52e29b86')
+            send_message(phone_number, message, '6715EDA2DBE4A', '670cf52e29b86')
 
         except Exception as e:
             print('Something went wrong')
